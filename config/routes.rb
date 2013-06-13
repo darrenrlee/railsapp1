0b9b35afa7e080e85b3e3f8dbc3d767e1610e0db
darren lee <darrenrlee@yahoo.com>
2013-06-13 16:48:38 -0400

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Railsapp1::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
